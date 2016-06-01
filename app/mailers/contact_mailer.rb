fe9d5ec33275bf48886456c53ef65684b1dc5859
Jorge Romussi <jorrope3@gmail.com>
2016-06-01 22:24:19 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form message')
+    end
+
+end
